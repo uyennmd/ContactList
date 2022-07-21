97cc972c2592c23ca1d7d3447dff44894bcaa081
Uyen <uyen.nguyenminhduy@gmail.com>
2022-07-21 13:22:42 +0700

    Initial Commit

---
+++ ContactList/ContactListApp.swift
@@ -0,0 +1,17 @@
+//
+//  ContactListApp.swift
+//  ContactList
+//
+//  Created by Uyen Nguyen Minh Duy on 21/07/2022.
+//
+
+import SwiftUI
+
+@main
+struct ContactListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
